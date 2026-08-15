85f12ff5e338abe7f9431a8776974a8d8fb278c8
philo0692 <phibrahim2023@gmail.com>
2026-08-15 03:37:02 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const C='mizan-v6';
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(C).then(c=>c.addAll(['index.html','manifest.json'])).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    if(res.ok&&e.request.url.startsWith(self.location.origin)){const cl=res.clone();caches.open(C).then(c=>c.put(e.request,cl));}
+    return res;
+  }).catch(()=>caches.match('index.html'))));
+});
